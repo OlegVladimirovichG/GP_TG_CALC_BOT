7d7178d1d032ed7e01d20603706b554b4180c950
Ivan Ashikhmin <sushkoos@gmail.com>
2022-11-23 10:42:24 +0400

    first commands

--- bot.py
+++ bot.py
@@ -14,7 +14,24 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(commands=["start"])
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer("Написать приветствие")
+
+
+# Хэндлер на команду /test1
+@dp.message(commands=["help"])
+async def cmd_test1(message: types.Message):
+    await message.reply("Написать помощ")
+
+
+# Хэндлер на команду /test2
+@dp.message(commands=["calc"])
+async def cmd_test2(message: types.Message):
+    await message.reply("Тут у нас калькулятор рациональных чисел")
+
+
+@dp.message(commands=["complex"])
+async def cmd_test2(message: types.Message):
+    await message.reply("Тут у нас калькулятор комплексных чисел")
 
 
 # Запуск процесса поллинга новых апдейтов
