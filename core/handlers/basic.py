317b7ad4c66bc933dec1d1afd67829868a9307a7
Ivan Ashikhmin <sushkoos@gmail.com>
2022-11-25 00:45:53 +0400

    Full calc logic

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -1,7 +1,10 @@
 from aiogram import Bot
 from aiogram.types import Message
-from core.keyboards.reply import get_reply_keyboard
 
 
 async def get_start(message: Message, bot: Bot):
-    await message.answer(f'Hello {message.from_user.first_name}', reply_markup=get_reply_keyboard())
+    await message.answer(f'Привет {message.from_user.first_name} бла бла')
+
+
+async def get_help(message: Message, bot: Bot):
+    await message.answer(f'Тут помощь')
