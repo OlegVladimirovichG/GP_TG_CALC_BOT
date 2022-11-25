3b65b8061ce74e4ea46814acae12a0d2f41df453
Ivan Ashikhmin <sushkoos@gmail.com>
2022-11-25 08:46:06 +0400

    Add custom handler

--- core/keyboards/reply.py
+++ core/keyboards/reply.py
@@ -6,7 +6,7 @@ def choose_calc():
     keyboard_builder.button(text='Рациональные')
     keyboard_builder.button(text='Комплексные')
     keyboard_builder.button(text='Свободное выражение')
-    keyboard_builder.adjust(3)
+    keyboard_builder.adjust(2, 1)
     return keyboard_builder.as_markup(resize_keyboard=True, one_time_keyboard=True,
                                       input_field_placeholder='Выбери кнопку ↓')
 
